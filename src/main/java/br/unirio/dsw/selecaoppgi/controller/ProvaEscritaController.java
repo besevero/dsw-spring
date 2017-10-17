0dc76b74baf42c4cf1638be6b0a212583e2a7d3f
Júlio Campos <juliogcampos@gmail.com>
2017-10-17 12:14:51 -0200

    #9 Identação código classe ProvaEscritaController

--- src/main/java/br/unirio/dsw/selecaoppgi/controller/ProvaEscritaController.java
+++ src/main/java/br/unirio/dsw/selecaoppgi/controller/ProvaEscritaController.java
@@ -1,7 +1,5 @@
 package br.unirio.dsw.selecaoppgi.controller;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,79 +14,74 @@ import br.unirio.dsw.selecaoppgi.model.inscricao.InscricaoEdital;
 import br.unirio.dsw.selecaoppgi.service.dao.InscricaoDAO;
 
 @Controller
-public class ProvaEscritaController
-{
-//	/edital/escrita/presenca
-//	/edital/escrita/nota
+public class ProvaEscritaController {
+	// /edital/escrita/presenca
+	// /edital/escrita/nota
 
-
-//	/edital/escrita/encerramento
+	// /edital/escrita/encerramento
 	InscricaoDAO inscricaoDAO = new InscricaoDAO();
-	
+
 	/*
-	 *Função que verifica quais candidatos possuem pendências
+	 * Função que verifica quais candidatos possuem pendências
 	 * 
 	 */
-	
-	public List<InscricaoEdital> VerificaCandidatosComPendenciaNasProvas(List<InscricaoEdital> ListaDeCanditados){
-		//TODO: Testar e Revisar
+
+	public List<InscricaoEdital> VerificaCandidatosComPendenciaNasProvas(List<InscricaoEdital> ListaDeCanditados) {
+		// TODO: Testar e Revisar
 		List<InscricaoEdital> candidatosComPendencia = new ArrayList<InscricaoEdital>();
-		
-		for(InscricaoEdital candidato : ListaDeCanditados){
-			for(AvaliacaoProvaEscrita provaEscrita : candidato.getAvaliacoesProvasEscritas()) {
-				
+
+		for (InscricaoEdital candidato : ListaDeCanditados) {
+			for (AvaliacaoProvaEscrita provaEscrita : candidato.getAvaliacoesProvasEscritas()) {
+
 				int idCandidato = candidato.getIdCandidato();
 				String codigoProva = provaEscrita.getProvaEscrita().getCodigo();
-				
-				if(inscricaoDAO.indicaAusenciaProvaEscrita(idCandidato, codigoProva)) {
+
+				if (inscricaoDAO.indicaAusenciaProvaEscrita(idCandidato, codigoProva)) {
 					candidatosComPendencia.add(candidato);
-				}
-				else{
+				} else {
 					CalculaNotaDaProvaEscrita(candidato);
 				}
 			}
 		}
 		return candidatosComPendencia;
 	}
-	
-	public void CalculaNotaDaProvaEscrita(InscricaoEdital candidato){
-  //calculo da prova
-	  ProvaEscrita provaEscrita = new ProvaEscrita();
-	  if(!provaEscrita.isDispensavel()) {
-    	  provaEscrita.adicionaQuestao(1);
-    	  provaEscrita.adicionaQuestao(1);
-    	  provaEscrita.adicionaQuestao(1);
-	  }
-	  else { return; }
-	  AvaliacaoProvaEscrita avaliacaoProvaEscrita = new AvaliacaoProvaEscrita(provaEscrita);
-	  avaliacaoProvaEscrita.setNotaOriginalQuestao(0, 50);
-	  avaliacaoProvaEscrita.setNotaOriginalQuestao(1, 80);
-	  avaliacaoProvaEscrita.setNotaOriginalQuestao(2, 100);
-	  
-	  for(int i=0; i<provaEscrita.contaQuestoes(); i++) {
-	    if(avaliacaoProvaEscrita.possuiNotaRecursoQuestao(i)) {
-	      avaliacaoProvaEscrita.setNotaOriginalQuestao(i, avaliacaoProvaEscrita.getNotaRecursoQuestao(i));
-	    }
-	  }
-	  
-	  int minhaNota = (avaliacaoProvaEscrita.getNotaOriginalQuestao(0) + avaliacaoProvaEscrita.getNotaOriginalQuestao(1) + avaliacaoProvaEscrita.getNotaOriginalQuestao(2))
-	  / provaEscrita.contaQuestoes();
-	  
-	  System.out.println(minhaNota >= provaEscrita.getNotaMinimaAprovacao() ? "aprovado":"reprovado");
-	}
-	
-	
-	
 
-    /**
-     * Ação que redireciona o usuário para a tela presença em prova escrita
-     */
+	public void CalculaNotaDaProvaEscrita(InscricaoEdital candidato) {
+		// Cálculo da prova
+		ProvaEscrita provaEscrita = new ProvaEscrita();
+		if (!provaEscrita.isDispensavel()) {
+			provaEscrita.adicionaQuestao(1);
+			provaEscrita.adicionaQuestao(1);
+			provaEscrita.adicionaQuestao(1);
+		} else {
+			return;
+		}
+		AvaliacaoProvaEscrita avaliacaoProvaEscrita = new AvaliacaoProvaEscrita(provaEscrita);
+		avaliacaoProvaEscrita.setNotaOriginalQuestao(0, 50);
+		avaliacaoProvaEscrita.setNotaOriginalQuestao(1, 80);
+		avaliacaoProvaEscrita.setNotaOriginalQuestao(2, 100);
+
+		for (int i = 0; i < provaEscrita.contaQuestoes(); i++) {
+			if (avaliacaoProvaEscrita.possuiNotaRecursoQuestao(i)) {
+				avaliacaoProvaEscrita.setNotaOriginalQuestao(i, avaliacaoProvaEscrita.getNotaRecursoQuestao(i));
+			}
+		}
+
+		int minhaNota = (avaliacaoProvaEscrita.getNotaOriginalQuestao(0)
+				+ avaliacaoProvaEscrita.getNotaOriginalQuestao(1) + avaliacaoProvaEscrita.getNotaOriginalQuestao(2))
+				/ provaEscrita.contaQuestoes();
+
+		System.out.println(minhaNota >= provaEscrita.getNotaMinimaAprovacao() ? "aprovado" : "reprovado");
+	}
+
+	/**
+	 * Ação que redireciona o usuário para a tela presença em prova escrita
+	 */
 	@RequestMapping(value = "/edital/escrita/presenca", method = RequestMethod.GET)
-	public ModelAndView mostraPaginaPresencaProvaEscrita()
-	{
+	public ModelAndView mostraPaginaPresencaProvaEscrita() {
 		ModelAndView model = new ModelAndView();
-        model.setViewName("edital/escrita/presenca");
+		model.setViewName("edital/escrita/presenca");
 		return model;
 	}
-	
+
 }
