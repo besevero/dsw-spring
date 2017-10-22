0efb6a0584848fc208823e5b467c0bbba23fffdd
Júlio Campos <juliogcampos@gmail.com>
2017-10-22 11:16:45 -0200

    #9 Corrigindo problema de merge e atualizando a classe TestInscricaoDAO.java

--- src/test/java/br/unirio/dsw/selecaoppgi/TestInscricaoDAO.java
+++ src/test/java/br/unirio/dsw/selecaoppgi/TestInscricaoDAO.java
@@ -1,72 +1,28 @@
 package br.unirio.dsw.selecaoppgi;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import br.unirio.dsw.selecaoppgi.controller.ProvaEscritaController;
 import br.unirio.dsw.selecaoppgi.model.edital.Edital;
 import br.unirio.dsw.selecaoppgi.model.inscricao.InscricaoEdital;
-import br.unirio.dsw.selecaoppgi.service.dao.InscricaoDAO;
 
-public class TestInscricaoDAO {
-	private InscricaoDAO inscricaoDAO;
+public class TestInscricaoDAO
+{
 
-	@Before
-<<<<<<< HEAD
-	public void setup()
+	@Test
+	public void test()
 	{
-	
-	}
-	
-
-	@Test//(expected = NullPointerException.class)
-	public void TestmarcaAprovadoProvasEscritas()
-	{
-	  //try {
-	    assertTrue(inscricaoDAO.marcaAprovadoProvasEscritas(1));
-	  /*}
-	  catch(Exception e) {
-	    e.getMessage();
-	  }*/
-=======
-	public void setup() {
->>>>>>> 521a71fbfdd55c690a0a571528824e017bbe9277
-
+		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testmarcaReprovadoProvasEscritas() {
-		assertTrue(inscricaoDAO.marcaReprovadoProvasEscritas(1) == true);
-	}
-
-	@Test
-	public void testCalculaNotaDaProvaEscrita() {
+	public void testCalculaNotaDaProvaEscrita()
+	{
 		ProvaEscritaController a = new ProvaEscritaController();
 		Edital edital = null;
 		InscricaoEdital candidato = new InscricaoEdital(edital);
-		a.CalculaNotaDaProvaEscrita(candidato);// retorna 76 de média
+		a.CalculaNotaDaProvaEscrita(candidato); // retorna 76 de média
 	}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 521a71fbfdd55c690a0a571528824e017bbe9277
-	@Test
-	public void testmarcaAprovadoProvasEscritas() throws Exception {
-		int idInscricao = 1;
-		try {
-			assertTrue(inscricaoDAO.marcaAprovadoProvasEscritas(idInscricao));
-
-		} catch (Exception e) {
-			fail("Ops! Deveria ter ocorrido uma exceção");
-		}
-	}
-<<<<<<< HEAD
-	
-=======
-
->>>>>>> 521a71fbfdd55c690a0a571528824e017bbe9277
 }
