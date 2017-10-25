e8503cc84caa3caf1f244baa6be71d9cf590eb6c
Alexsander Nolasco <alexsander.reef@hotmail.com>
2017-10-24 23:41:00 -0200

    #16 programando encerramento de alinhamento

--- src/main/java/br/unirio/dsw/selecaoppgi/controller/ProvaAlinhamentoController.java
+++ src/main/java/br/unirio/dsw/selecaoppgi/controller/ProvaAlinhamentoController.java
@@ -1,6 +1,11 @@
 package br.unirio.dsw.selecaoppgi.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+
+import br.unirio.dsw.selecaoppgi.model.inscricao.AvaliacaoProvaEscrita;
+import br.unirio.dsw.selecaoppgi.model.inscricao.InscricaoEdital;
 
 @Controller
 public class ProvaAlinhamentoController
@@ -8,4 +13,22 @@ public class ProvaAlinhamentoController
 //	/edital/alinhamento/presenca
 //	/edital/alinhamento/notas
 //	/edital/alinhamento/encerramento
+  public void encerrarAlianhamentoDeCandidato(List<InscricaoEdital> ListaDeCanditados) {
+    //List<InscricaoEdital> candidatos = new ArrayList<InscricaoEdital>();
+    
+    for (InscricaoEdital candidato : ListaDeCanditados) {
+      for (AvaliacaoProvaEscrita provaEscrita : candidato.getAvaliacoesProvasEscritas()) {
+        int idCandidato = candidato.getIdCandidato();
+        //marcaAprovadoProvasEscritas(idCandidato) -> identifica estado de aprovação do candidato
+        if(provaEscrita.getProvaEscrita() != null && candidato.exigeProvaOral()){
+       // TODO: mover edital para estado de finalizado, calcular nota do candidato e salvar no banco de dados
+        }
+        else {
+          //List<candidato.getNomeCandidato> candidatos = new ArrayList<getNomeCandidato>();
+          candidato.getNomeCandidato();
+
+        }
+      }
+    }
+  }
 }
