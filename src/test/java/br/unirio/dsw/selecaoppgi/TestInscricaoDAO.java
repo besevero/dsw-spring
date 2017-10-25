2c8b48283c8c618d3842e751ce7a81fca6efcc95
Júlio Campos <juliogcampos@gmail.com>
2017-10-25 01:05:58 -0200

    Resolvendo conflito de merge na classe TestInscricaoDAO

--- src/test/java/br/unirio/dsw/selecaoppgi/TestInscricaoDAO.java
+++ src/test/java/br/unirio/dsw/selecaoppgi/TestInscricaoDAO.java
@@ -8,33 +8,8 @@ import br.unirio.dsw.selecaoppgi.controller.ProvaEscritaController;
 import br.unirio.dsw.selecaoppgi.model.edital.Edital;
 import br.unirio.dsw.selecaoppgi.model.inscricao.InscricaoEdital;
 
-<<<<<<< HEAD
-public class TestInscricaoDAO {
-	private InscricaoDAO inscricaoDAO;
-
-	/*@Before
-	public void setup()
-	{
-	
-	}
-	
-
-	@Test//(expected = NullPointerException.class)
-	public void TestmarcaAprovadoProvasEscritas()
-	{
-	  //try {
-	    assertTrue(inscricaoDAO.marcaAprovadoProvasEscritas(1));
-	  /*}
-	  catch(Exception e) {
-	    e.getMessage();
-	  }*/
-	public void setup() {
-
-	}
-=======
 public class TestInscricaoDAO
 {
->>>>>>> 15635db5675b1d913202487a6cf50df670efa0e8
 
 	@Test
 	public void test()
@@ -48,24 +23,6 @@ public class TestInscricaoDAO
 		ProvaEscritaController a = new ProvaEscritaController();
 		Edital edital = null;
 		InscricaoEdital candidato = new InscricaoEdital(edital);
-<<<<<<< HEAD
-		a.CalculaNotaDaProvaEscrita(candidato);// retorna 76 de média apenas
-	}
-
-	@Test
-	public void testmarcaAprovadoProvasEscritas() throws Exception {
-		int idInscricao = 1;
-		try {
-			assertTrue(inscricaoDAO.marcaAprovadoProvasEscritas(idInscricao));
-
-		} catch (Exception e) {
-			fail("Ops! Deveria ter ocorrido uma exceção");
-		}
-	}
-
-}
-=======
 		a.CalculaNotaDaProvaEscrita(candidato); // retorna 76 de média
 	}
 }
->>>>>>> 15635db5675b1d913202487a6cf50df670efa0e8
