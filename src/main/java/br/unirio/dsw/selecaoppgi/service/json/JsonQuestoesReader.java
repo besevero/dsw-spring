dafa771ba59d4750bc373cc7abd87ba902acd9cc
besevero <besevero@gmail.com>
2017-10-26 17:17:45 -0200

    Correções

--- src/main/java/br/unirio/dsw/selecaoppgi/service/json/JsonQuestoesReader.java
+++ src/main/java/br/unirio/dsw/selecaoppgi/service/json/JsonQuestoesReader.java
@@ -3,6 +3,15 @@ package br.unirio.dsw.selecaoppgi.service.json;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+
+/**
+ * Classe responsável por carregar as notas das questões a partir da sua representação JSON
+ * 
+ * @author Bernardo Severo
+ */
+
 public class JsonQuestoesReader {
+	
+	
 
 }
--- src/main/java/br/unirio/dsw/selecaoppgi/service/json/JsonQuestoesWritter.java
+++ src/main/java/br/unirio/dsw/selecaoppgi/service/json/JsonQuestoesWritter.java
@@ -3,6 +3,13 @@ package br.unirio.dsw.selecaoppgi.service.json;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+
+/**
+ * Classe que publica as notas das questões da prova em formato JSON
+ * 
+ * @author Bernardo Severo
+ */
+
 public class JsonQuestoesWritter {
 
 }
